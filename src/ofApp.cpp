12112b90a5b74a4143ca3a189178c9c89e97403a
Rikiya89 <83910358+Rikiya89@users.noreply.github.com>
2022-12-04 13:59:17 +0900

    Update ofApp.cpp

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,20 +3,9 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
 
-    bufSize = 256;//buffer size
-    ofBackground(33, 33, 33);
-    ofSetColor(0, 0, 255);
-
-    //setting for play music
-    ofSoundStreamSettings settings;
-    settings.setOutListener(this);
-    settings.numOutputChannels = 2;
-    settings.numInputChannels = 0;
-    settings.bufferSize = 512;
-    soundStream.setup(settings);
-
-    left = new float[bufSize];
-    right = new float [bufSize] ;
+    mySound.load("lifelike-126735.mp3");
+    mySound.setLoop(true);//on loop
+    mySound.play();//sound play
 }
 
 //--------------------------------------------------------------
@@ -26,34 +15,6 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    float audioHeight = ofGetHeight()/2.0f;
-    float phaseDiff = ofGetWidth()/float(bufSize);
-
-    ofSetColor(0, 204, 0);
-    ofNoFill();
-    ofSetLineWidth(3);
-
-    //draw left channel waveform
-    ofBeginShape();
-    for (int i = 0; i < bufSize; i++){
-        ofVertex(i * phaseDiff, audioHeight/2 + left[i] * audioHeight);
-    }
-    ofEndShape();
-
-    //draw right channel waveform
-    ofBeginShape();
-    for (int i = 0; i < bufSize; i++){
-        ofVertex(i * phaseDiff, audioHeight / 2 * 3 + right[i] * audioHeight);
-    }
-    ofEndShape();
-}
-
-void ofApp::audioReceived (float * input, int bufferSize, int nChannels){
-    //Input a voice
-    for (int i = 0; i < bufferSize; i++){
-        left[i] = input[i*2];
-        right[i] = input[i*2+1];
-    }
 }
 
 //--------------------------------------------------------------
@@ -71,17 +32,23 @@ void ofApp::mouseMoved(int x, int y ){
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-
+    //setting for pan
+    mySound.setPan(x / (float)ofGetWidth() * 2 - 1.0f);
+    //change to play speed
+    mySound.setSpeed(0.5f + ((float)(ofGetHeight() - y) / (float)ofGetHeight()) * 1.0f);
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+    //setting for pan
+    mySound.setPan(x / (float)ofGetWidth() * 2 - 1.0f);
+    mySound.setSpeed(0.5f + ((float)(ofGetHeight() - y) / (float)ofGetHeight()) * 1.0f);
+    mySound.play();
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    mySound.stop();
 }
 
 //--------------------------------------------------------------
